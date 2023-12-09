42e61ac6b57ac701189d3236198effaff4e6e038
DipuYadav5253 <dipuyadav5253@gmail.com>
2023-12-09 23:05:53 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        [req.replace("\n","") for req in  requirements]
+        return requirements
+
+
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='Dipu',
+    author_email='dipuyadav5253@gmail.com',
+    install_reuires=get_requirements('requirements.txt'),
+    packages=find_packages()
+
+)
